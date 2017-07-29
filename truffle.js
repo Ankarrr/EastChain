e044d02502b37deaa46eadc15816f63e952a7b59
Ankarrr <twan0817@gmail.com>
2017-07-29 15:32:52 +0800

    Add new network AWS
    - connect to geth RPC in 54.65.92.97:5566

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    AWS: {
+      host: "54.65.92.97",
+      port: 5566,
+      network_id: 55661
     }
   }
 };
