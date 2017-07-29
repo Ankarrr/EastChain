6868cbb20b7a2a5b4e309fb35eeffae75ba65b54
Ankarrr <twan0817@gmail.com>
2017-07-29 13:46:16 +0800

    Initial commit and Add smart contract Wine.sol
    - Use Truffle.js
    - Add smart contract Wine.sol with simple create function

---
+++ contracts/Wine.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.4;
+
+contract Wine {
+    struct  Sorghum{
+        address owner;
+        string name;
+        string productType;
+        string date;
+        string origin;
+        uint price;
+        string purpose; // e.g. general, wedding
+        string description;
+    }
+    
+    address public creator;
+    // (barcode => Sorghum)
+    mapping (uint => Sorghum) public SorgHums;
+
+    function Wine() {  
+        creator = msg.sender;    
+    }
+    
+    function createSorghum(
+        uint barcode,
+        address owner,
+        string name,
+        string productType,
+        string date,
+        string origin,
+        uint price,
+        string purpose,
+        string description) {
+        SorgHums[barcode] = Sorghum(
+            owner, name, productType, date, origin, price, purpose, description);
+    }
+}
